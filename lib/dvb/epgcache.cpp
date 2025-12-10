174b1e356fab615d6b319fbf0c0d7e6af1c04d85
blinkosm10 <rico2clay@gmail.com>
2025-12-10 18:01:54 +0100

    Update epgcache.cpp

--- lib/dvb/epgcache.cpp
+++ lib/dvb/epgcache.cpp
@@ -2115,6 +2115,12 @@ void eEPGCache::submitEventData(const std::vector<int>& sids, const std::vector<
 	}
 }
 
+void eEPGCache::setEpgmaxdays(unsigned int epgmaxdays)
+
+{
+        maxdays = epgmaxdays;
+} 
+
 void eEPGCache::setEpgHistorySeconds(time_t seconds)
 {
 	historySeconds = seconds;
